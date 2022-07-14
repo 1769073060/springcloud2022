587837ffc41ed2a1c5504cf2725de1562c243015
Rzk <1769073060@qq.com>
2022-07-14 11:01:11 +0800

    update:修改eureka服务

--- springcloud-eureka-server7012/src/main/java/com/rzk/eureka/EurekaMain7012.java
+++ springcloud-eureka-server7012/src/main/java/com/rzk/eureka/EurekaMain7012.java
@@ -1,2 +1,15 @@
-package com.rzk.eureka;public class EurekaMain7012 {
+package com.rzk.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaMain7012 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7012.class,args);
+    }
+
 }
