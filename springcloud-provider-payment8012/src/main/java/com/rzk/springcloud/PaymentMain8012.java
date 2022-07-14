900c556aa6548f8637e89b1355cd0bd0cb2f915c
Rzk <1769073060@qq.com>
2022-07-14 11:00:21 +0800

    add:添加服务提供者8012服务

--- springcloud-provider-payment8012/src/main/java/com/rzk/springcloud/PaymentMain8012.java
+++ springcloud-provider-payment8012/src/main/java/com/rzk/springcloud/PaymentMain8012.java
@@ -1,2 +1,17 @@
-package com.rzk.springcloud;public class PaymentMain8012 {
+package com.rzk.springcloud;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableDiscoveryClient
+@EnableEurekaClient
+@MapperScan("com.rzk.springcloud.dao")
+@SpringBootApplication
+public class PaymentMain8012 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8012.class,args);
+    }
 }
